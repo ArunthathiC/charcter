eea326fb5a9a86f8b1a31e18cb8599658892ea2b
Arunthathi <arunthi19@gmail.com>
2016-07-09 10:33:35 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+  char a;
+  printf("Enter the character");
+  scanf("%c",&ab);
+  if(ab>='a'b&&ab<='z')&&(ab>='A'&&ab<='Z')
+    printf("The character is alphabet");
+  else
+    printf("The character is not alphabet");
+  }
